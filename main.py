48f5a63eaf106b618702ff654af3cbe4bf6fcaea
maxime-antoine <maxime.antoine001@gmail.com>
2023-09-13 15:26:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,121 @@
+import csv
+import os
+from datetime import datetime
+
+carsDb = []
+allCars = []
+newCars = []
+oldCars = []
+
+scriptCarPrices = ""
+
+def findCar(brand, model):
+    res = []
+    # We look for the exact car
+    for car in allCars:
+        if (car['brand'].lower() == brand.lower()) and (car['model'].lower() == model.lower()):
+            res.append(car)
+    # if no cars were found, we look for cars that contain the brand and model
+    if (len(res) == 0):
+        for car in allCars :
+            if (car['brand'].lower() in brand.lower() or brand.lower() in car['brand'].lower()) and (car['model'].lower() in model.lower() or model.lower() in car['model'].lower()):
+                res.append(car)
+    return res
+
+def getAvgData(brand, model) :
+    res = findCar(brand, model)
+    if (len(res) == 0):
+        return None
+    avgPrice = 0
+    avgMileage = 0
+    for car in res:
+        avgPrice += float(car['price'])
+        avgMileage += float(car['mileage'])
+    avgPrice /= len(res)
+    avgMileage /= len(res)
+    return {
+        'avgPrice': round(avgPrice),
+        'avgMileage': round(avgMileage),
+        'cars' : res
+    }
+
+with open('data/data2023.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    id = 1
+    for line in csv_reader:
+        line = line[0].split(';')
+
+        car = {
+            'id': id,
+            'brand': line[2],
+            'model': line[3],
+            'avgPrice': 0,
+        }
+
+        carsDb.append(car)
+        id += 1
+
+with open('data/prices.csv', 'r', encoding="utf8") as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for line in csv_reader:
+        # ignore first line
+        if (line[0] == 'ID'):
+            continue
+
+        mileage = line[10].replace(' km', '')
+
+        carPrice = {
+            'brand': line[3],
+            'model': line[4],
+            'year': line[5],
+            'price': line[1],
+            'mileage': mileage,
+        }
+
+        allCars.append(carPrice)
+
+        if (float(mileage) <= 20):
+            newCars.append(carPrice)
+        else:
+            oldCars.append(carPrice)
+
+for car in carsDb:
+    avgData = getAvgData(car['brand'], car['model'])
+    if (avgData == None):
+        continue
+    car['avgPrice'] = avgData['avgPrice']
+    car['avgMileage'] = avgData['avgMileage']
+
+    for carPrice in avgData['cars']:
+        scriptCarPrices += "INSERT INTO car_price (car_id, price, mileage, year) VALUES (" + str(car['id']) + ", " + carPrice['price'] + ", " + carPrice['mileage'] + ", " + carPrice['year'] + ");\n"
+
+
+# move the files from the "exports" directory to the "old_exports" directory
+
+for file in os.listdir('exports'):
+    os.rename('exports/' + file, 'old_exports/' + file)
+
+# We create the sql script fro the cars_th table
+
+script = ""
+for car in carsDb :
+    if (car['avgPrice'] != 0):
+        script += "UPDATE car_th SET price_used = " + str(car['avgPrice']) + " WHERE id = " + str(car['id']) + ";\n"
+
+# We create the file
+
+now = datetime.now()
+fileName = "carsTH-price-" + now.strftime("%d%m%Y%H%M%S") + ".sql"
+file = open('exports/' + fileName, 'w')
+file.write(script)
+file.close()
+
+# We create the sql script for the car_price table
+
+now = datetime.now()
+fileName = "old-carsTH" + now.strftime("%d%m%Y%H%M%S") + ".sql"
+file = open('exports/' + fileName, 'w')
+file.write(scriptCarPrices)
+file.close()
